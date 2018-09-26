2f4b76b1e0fcb2104f38cc93b751de7f87caf7f7
yao <1368607459@qq.com>
2018-09-26 23:08:07 +0800

    把爬虫脚本放到Django目录中，每次运行project都会爬一次新闻更新到网站中。

--- cityzen/views.py
+++ cityzen/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, HttpResponse
 from django.core.paginator import Paginator,Page
 from cityzen.models import n_163, sina, qq
+from cityzen.Manchester_City import Man_City
+M = Man_City()
 # Create your views here.
 
 def get_db(request):
+    M.main()
     db_sina = sina.objects.order_by('-pub_date')[:60]
     db_163 = n_163.objects.order_by('-pub_date')[:60]
     db_qq = qq.objects.order_by('-pub_date')[:60]
